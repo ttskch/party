5cbddadab25c2c88eacb191145b6c0d7ebbc3d12
David Sosa Valdes <david.sosa.valdes@gmail.com>
2016-02-08 11:48:49 -0600

    Create mx.php

---
+++ units/mx.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'num_per_head' => [
+        'pizza' => 0.4,
+        'alcohol' => 2.5,
+        'non_alcohol' => 0.6,
+    ],
+    // Mexican pesos
+    'unit_price' => [
+        'pizza' => 150,
+        'alcohol' => 100,
+        'non_alcohol' => 50,
+    ],
+];
